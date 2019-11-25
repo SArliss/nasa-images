a178edbaf9256c28947b10c09272dc6f35d84eaa
sinaraarliss <sinaramartins@gmail.com>
2019-11-25 17:14:42 -0500

    Function renderResult: Selecting elements from results array

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
-
 window.onload = function () {
+
+
 
   // Setting all my global variables
   const button = document.querySelector("button");
+  // Selecting the whole <input id="mainInput" type="text" placeholder="Type here"> 
   const search = document.querySelector("input");
-  console.log(search);
+
 
 
   // NASA API Request 
@@ -20,27 +21,27 @@ window.onload = function () {
     console.log(results);
 
     // Calling the renderResults function and passing the searchPicked results as a parameter. Two functions: Better usability 
-    //renderResults(results)
-
-
-    // Items index value will loop from i = 0 to items.length. Links index value is fixed to 0. 
-    // const imgUrl = results.data.collection.items[1].links[0].href;
-    // const title = results.data.collection.items[1].data[0].title;
-    // const nasaID = results.data.collection.items[1].data[0].nasa_id;
-    // const year = results.data.collection.items[1].data[0].date_created;
-    // const description = results.data.collection.items[1].data[0].description;
-
-    // console.log(imgUrl);
-    // console.log(title);
-    // console.log(nasaID);
-    // console.log(year);
-    // console.log(description);
-
-
+    renderResults(results)
 
   })
 
 
+  function renderResults(results) {
+    // Items index value will loop from i = 0 to items.length. Links and data index values are fixed to 0. 
+    // Or until items.lenght lesser than 20 
+    const imgUrl = results.data.collection.items[1].links[0].href;
+    const title = results.data.collection.items[1].data[0].title;
+    const nasaID = results.data.collection.items[1].data[0].nasa_id;
+    const year = results.data.collection.items[1].data[0].date_created;
+    const description = results.data.collection.items[1].data[0].description;
+
+    console.log(imgUrl);
+    console.log(title);
+    console.log(nasaID);
+    console.log(year);
+    console.log(description);
+
+  }
 
 
 
